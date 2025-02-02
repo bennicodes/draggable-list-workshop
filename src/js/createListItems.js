865afb5c2ef5718f6590e80344a9237d8a834f8b
Benedict Notoane <benni@MacBookPro.lan>
2025-02-02 19:31:41 +0100

    implemented compare order feature

--- src/js/createListItems.js
+++ src/js/createListItems.js
@@ -1,3 +1,4 @@
+import compareOrder from "./compareOrder.js";
 import handleDragAndDrop from "./dragAndDrop.js";
 import shuffleArray from "./shuffleArray.js";
 import stepsForSoftwareProject from "./stepsData.js";
@@ -10,7 +11,7 @@ const createListItems = () => {
   shuffledArray.forEach((step, index) => {
     const listItem = document.createElement("li");
     list.append(listItem);
-    listItem.textContent = `${index + 1}. ${step}`;
+    listItem.textContent = `${index + 1}.${step}`;
     listItem.classList.add("list__item");
     listItem.setAttribute("draggable", true);
     listItem.setAttribute("data.index", index);
@@ -18,8 +19,18 @@ const createListItems = () => {
   const compareButton = document.createElement("button");
   document.querySelector(".main-container").append(compareButton);
   compareButton.textContent = "Check order";
-  compareButton.classList.add("list__compare-button");
-  //   compareButton.addEventListener("click", compareOrder);
+  compareButton.classList.add("list__button");
+  compareButton.addEventListener("click", compareOrder);
+  handleDragAndDrop();
+
+  const restartButton = document.createElement("button");
+  document.querySelector(".main-container").append(restartButton);
+  restartButton.textContent = "Try Again";
+  restartButton.classList.add("list__button");
+
+  restartButton.addEventListener("click", () => {
+    location.reload();
+  });
   handleDragAndDrop();
 };
 
